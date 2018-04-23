f40f0c3005fd9f54297d5b3a222820e5f4267f27
Napier40401620 <zhtun408@gmail.com>
2018-04-23 14:53:28 +0630

    add inherits_file

--- test2.py
+++ test2.py
@@ -7,4 +7,4 @@ def hello(name=None):
 	return render_template('test2.html', name=name)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5001, debug=True)
+	app.run(host='0.0.0.0', port=5000, debug=True)
